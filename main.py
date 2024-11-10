20168050eb661d47588288eb90622e425479636a
Ori <disori777@gmail.com>
2024-11-10 10:29:49 +0800

    Add break to clean out code, and put dictionary into a variable to decouple

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/api/v1/<word>')
 def definition(word):
     word = word
     word_definition = ""
-    return {"definition": word_definition,
+    word_dict = {"definition": word.upper(),
             "word": word}
+    return word_dict
+
+
 if __name__ == "__main__":
     app.run(debug=True)
