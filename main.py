af6d0e777a86918abea75db87213ebda860ce6ac
Ori <disori777@gmail.com>
2024-11-11 22:15:21 +0800

    Place word_df outside function api. Reason in comment below it.

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
-
+word_df = pd.read_csv('data/dictionary.csv')
+# The reason why I put word_df outside the function scope is because we do not need to change the df each time a url
+# is called, since all the words is in one csv file. That is the main difference with the weather-api that I just built.
+# While it would still work if I put it inside api, it does not fit in there, whereas in weather-api, the data file
+# We need to access is dynamic, meaning each station's data file is different from one another, thus we need to put
+# it inside the function call to dynamically change the df based on the station inputted by the user
 
 @app.route('/')
 def home():
@@ -11,7 +16,6 @@ def home():
 
 @app.route('/api/v1/<word>')
 def api(word):
-    word_df = pd.read_csv('data/dictionary.csv')
     word_definition = word_df.loc[word_df['word'] == word]['definition'].squeeze()
     word_dict = {"definition": word_definition,
             "word": word}
